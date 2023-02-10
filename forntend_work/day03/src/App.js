ff1c8415046b01b3286deb596cf617d7e3768589
DESKTOP-VPE9I76\SW중심대학사업단_24 <sth7492@naver.com>
2023-02-10 16:51:25 +0900

    2023-02-10 (오후)

--- forntend_work/day03/src/App.js
+++ forntend_work/day03/src/App.js
@@ -1,37 +1,26 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Test from "./test";
+import React, { useState, useEffect } from 'react';
+// CSS는 App.css 파일을 만들어 스타일 적용 후 App.js에서 import하면 된다.
+
+function Menu(props) {
+    const [menuItems, setMenuItems] = useState(['HOME', 'PROFILE', 'GALLERY', 'LECTURE', 'GUEST']);
+    return (
+        <>
+            <ul className="menu">
+                {menuItems.map((item, i) => {
+                    return <li key="i">item</li>;
+                })}
+            </ul>
+        </>
+    );
+}
 
 function App() {
-  const [saramList, setSaramList] = useState([]);
-  // 여기서 saramList 는 스테이트라고 한다?
-  // setSaramList 는 setter ??
-
-  // useEffect 라는 훅?
-  useEffect( ()=> {
-    
-    // axios 모듈을 활용한 Ajax 처리
-    axios.get("http://localhost:5000/car").then((response)=>{
-      setSaramList(response.data);
-    });
-  },[]);
-
-  return (
-
-    // html 처럼 보이지만 JSX 이다. 
-    // {여기에 자바스크립트 문법 써} 안에 넣으면 됨
-    <div>
-      <h1>길동이의 홈페이지</h1>
-      <Test></Test>
-      <ul>
-        {
-          saramList.map((saram)=>{
-            return <li key={saram.no}>{saram.name},{saram.company},{saram.year}</li>
-          })
-        }
-      </ul>
-    </div>
-  );
+    return (
+        <>
+            <h1>Comstudy SW Schools</h1>
+            <Menu></Menu>
+        </>
+    );
 }
 
 export default App;
